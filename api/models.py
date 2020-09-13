cb764e330b58ec8b36ce83815a436db6c1c07760
constantindjonkam <70917662+constantindjonkam@users.noreply.github.com>
2020-09-12 17:05:27 -0700

    Add files via upload

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from tastypie.resources import ModelResource
+from movies.models import Movie
+
+
+class MovieResource(ModelResource):
+    class Meta:
+        queryset = Movie.objects.all()
+        resource_name = 'movies'
+        excludes = ['date_created']
