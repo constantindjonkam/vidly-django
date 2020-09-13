5a4e483eb555e70cfb39e14247459a5fecf303ba
constantindjonkam <70917662+constantindjonkam@users.noreply.github.com>
2020-09-12 17:06:34 -0700

    Add files via upload

---
+++ movies/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Genre, Movie
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ('date_created', )
+    list_display = ('title', 'genre', 'number_in_stock', 'daily_rate')
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
